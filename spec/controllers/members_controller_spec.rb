d5c40f45c623057feab7cfc26f72190265af660a
Jason T. Wong <developer@jasontwong.com>
2013-05-16 17:51:19 -0500

    Added some spec tests for members controller

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe MembersController do
+  describe 'GET #index' do
+    it 'populates an array of members' do
+      member = FactoryGirl.create(:member)
+      get :index
+      assigns(:members).should eq([member])
+    end
+    it 'returns json encoded version' do
+      get :index
+      response.status.should == 200
+    end
+  end
 
+  describe 'GET #show' do
+    it 'assigns the requested member to @member'
+    it 'returns json encoded version'
+  end
+
+  describe 'POST #create' do
+    context 'with valid attribtues' do
+      it 'creates a new user in the database'
+      it 'returns json encoded version'
+    end
+  end
+  it 'sends an update command'
+  it 'sends a delete command'
 end
