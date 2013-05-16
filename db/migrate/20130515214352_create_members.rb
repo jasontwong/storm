0bb25187b7b2ebcfbc60d2f9123f7b2c3b7885fd
Jason T. Wong <developer@jasontwong.com>
2013-05-16 10:09:04 -0500

    Added Member migration

--- db/migrate/20130515214352_create_members.rb
+++ db/migrate/20130515214352_create_members.rb
@@ -1,6 +1,12 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
+      t.string :email
+      t.string :password
+      t.string :salt
+      t.string :fb_username
+      t.string :fb_password
+      t.integer :active
 
       t.timestamps
     end
