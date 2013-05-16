55efb8b5c059f3c603f0e565711acd9c984357eb
Jason T. Wong <developer@jasontwong.com>
2013-05-16 11:11:43 -0500

    Added generator configuration to application

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module DataApi
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+      g.test_framework :rspec,
+      fixtures: true
+    end
   end
 end
