7ba21a5314894b83699fbb135f88730289b8d7df
Jason T. Wong <developer@jasontwong.com>
2013-05-16 14:56:13 -0500

    Removed all model validation (should be done by workers)

--- spec/models/member_attribute_spec.rb
+++ spec/models/member_attribute_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe MemberAttribute do
-  it 'requires a member' do
-    FactoryGirl.build(:member_attribute, member: nil).should_not be_valid
-  end
-  it 'requires a name' do
-    FactoryGirl.build(:member_attribute, name: nil).should_not be_valid
-  end
-  it 'requires a unique member/name combination' do
-    member = FactoryGirl.build(:member)
-    attr1 = FactoryGirl.create(:member_attribute, name: 'hello', member: member)
-    attr2 = FactoryGirl.build(:member_attribute, name: 'hello', member: member)
-    attr2.should_not be_valid
+  it 'has a valid factory' do
+    FactoryGirl.build(:member_attribute).should be_valid
   end
 end
