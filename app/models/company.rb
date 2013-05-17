02b5382dbfb7d66284ace706a08096889b2a5937
Jason T. Wong <developer@jasontwong.com>
2013-05-17 18:25:17 -0500

    Added products API Updated company model to add association to products

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ActiveRecord::Base
   attr_accessible :description, :location, :logo, :name, :phone
 
   has_many :stores
+  has_many :products
 
   validates :name, presence: true
 end
