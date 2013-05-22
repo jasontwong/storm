571f860248c9e777ea5a570a94d9a300fab358e6
Jason T. Wong <developer@jasontwong.com>
2013-05-22 16:50:26 -0500

    Added code api

---
+++ spec/models/code_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Code do
+  it 'has a valid factory' do
+    FactoryGirl.build(:code).should be_valid
+  end
+  it 'requires a qr' do
+    FactoryGirl.build(:code, qr: nil).should_not be_valid
+  end
+  it 'requires used' do
+    FactoryGirl.build(:code, used: nil).should_not be_valid
+  end
+  it 'requires active to be true or false' do
+    FactoryGirl.build(:code, active: 5).active.should be_false
+    FactoryGirl.build(:code, active: 'a').active.should be_false
+    FactoryGirl.build(:code, active: 1).should be_valid
+    FactoryGirl.build(:code, active: 0).should be_valid
+  end
+end
