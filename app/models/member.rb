c9d2eeb6722e32eebd196904ff7156c72251d492
Jason T. Wong <developer@jasontwong.com>
2013-05-24 13:55:58 -0500

    Removed Amazon SQS for reducing dev time

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,17 +10,4 @@ class Member < ActiveRecord::Base
 
   validates :email, :uniqueness => true, :presence => true, :email => true
   validates :active, :inclusion => { :in => [true, false] }
-
-  # this will send messages to AWS SQS member queue
-  def self.message(action, data)
-    # validate data
-    user = {
-      action: action,
-      user: data,
-      time: Time.now.to_i,
-    }
-    sqs = AWS::SQS.new(:region => ENV['AWS_REGION'])
-    queue = sqs.queues.create('member')
-    msg = queue.send_message(user.to_json)
-  end
 end
