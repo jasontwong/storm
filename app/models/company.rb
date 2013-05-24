d2855d1dc4cfa2ce665000d2412ae642ef1f7f57
Jason T. Wong <developer@jasontwong.com>
2013-05-24 11:27:03 -0500

    Added survey question limits to companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
-  attr_accessible :description, :location, :logo, :name, :phone
+  attr_accessible :description, :location, :logo, :name, :phone, :survey_question_limit
 
   has_many :stores, inverse_of: :company
   has_many :products, inverse_of: :company
@@ -9,4 +9,10 @@ class Company < ActiveRecord::Base
   has_many :member_points, inverse_of: :company
 
   validates :name, presence: true
+
+  after_initialize :init
+
+  def init
+    self.survey_question_limit = 5 if self.survey_question_limit.nil?
+  end
 end
