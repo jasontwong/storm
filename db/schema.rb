f628cf3617e56d87177ec793915ee20d8e56a7a6
Jason T. Wong <developer@jasontwong.com>
2013-06-27 10:44:28 -0500

    Add text column to codes to hold receipt data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130622185319) do
+ActiveRecord::Schema.define(:version => 20130627154320) do
 
   create_table "api_keys", :force => true do |t|
     t.string   "access_token"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20130622185319) do
     t.integer  "used"
     t.boolean  "active"
     t.datetime "last_used_time"
+    t.text     "text"
   end
 
   create_table "companies", :force => true do |t|
