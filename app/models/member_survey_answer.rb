6716845edf9f9b81e92e5f9d71f2c8b20d209c67
Jason T. Wong <developer@jasontwong.com>
2013-07-04 14:32:32 -0500

    Added survey_question_id to member_survey_answer Added relationship matching between survey_question and member_survey_answer

--- app/models/member_survey_answer.rb
+++ app/models/member_survey_answer.rb
@@ -1,7 +1,8 @@
 class MemberSurveyAnswer < ActiveRecord::Base
-  attr_accessible :answer, :member_survey_id, :question
+  attr_accessible :answer, :member_survey_id, :question, :survey_question_id
 
   belongs_to :member_survey, inverse_of: :member_survey_answers
+  belongs_to :survey_question, inverse_of: :member_survey_answers
 
   validates :member_survey_id, presence: true
   validates :question, presence: true
