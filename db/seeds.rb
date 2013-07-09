1a4f75a4b6e890e6d5e17455108d46e633fd9121
Jason T. Wong <developer@jasontwong.com>
2013-07-09 10:55:13 -0500

    Updated seed data for production/development scenarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,150 +12,242 @@ def random_num (decimal = false)
   lambda { |min, max| rand * (max - min) + min }.call(100, 1).round(round)
 end
 
-# Rewards
-# Companies
-# Surveys
+case Rails.env
+when 'development'
+  # test seed
 
-ApiKey.delete_all
-ApiKey.create!(access_token: 'apikey')
+  ApiKey.delete_all
+  ApiKey.create!(access_token: 'apikey')
 
-5.times do
+  5.times do
+    company = Company.create!(
+      name: Faker::Company.name,
+      logo: { 
+        large: 'bigimage', 
+        small: 'smallimage',
+        bg: 'bgimage',
+        colors: {
+          header: {
+            r: random_num,
+            g: random_num,
+            b: random_num,
+          },
+          points: {
+            r: random_num,
+            g: random_num,
+            b: random_num,
+          },
+          reward: {
+            r: random_num,
+            g: random_num,
+            b: random_num,
+          },
+        },
+      },
+      description: Faker::Lorem.sentence,
+      location: Faker::Address.street_address(true),
+      phone: Faker::PhoneNumber.phone_number,
+      survey_question_limit: 6,
+      html: '<!DOCTYPE html><html><head></head><body></body><p style="text-align:center;">Yella <strong>bold</strong> <em>italic</em>!</p><p style="text-align:center;width:100%;">{QR}</p><div style="float:left;width:20%;">float left</div><div style="float:right;width:20%;">float right</div><div style="clear:both;">Cleared</div><div><p>Mongoose:</p><hr /><img src="http://www.enchantedlearning.com/ygifs/Yellowmongoose_bw.GIF" /></div><div><ul><li>A list item</li><li style="color:red;">Print red?</li><li><ul><li>A nested list item</li></ul></li></ul></div><table><tbody><tr><td>Support table cells?</td><td>Support table cells?</td></tr></tbody></table></html>',
+    )
+    5.times do |num|
+      store = Store.create!(
+        address1: Faker::Address.street_address,
+        city: Faker::Address.city,
+        company_id: company.id,
+        country: Faker::Address.country,
+        latitude: Faker::Address.latitude,
+        longitude: Faker::Address.longitude,
+        name: 'Store ' + num.to_s,
+        phone: Faker::PhoneNumber.phone_number,
+        state: Faker::Address.state,
+        zip: Faker::Address.zip_code,
+      )
+      5.times do |num_a|
+        survey = Survey.create!(
+          title: "Survey Type " + num_a.to_s,
+          store_id: store.id,
+          description: Faker::Lorem.sentence,
+          default: false,
+        )
+        10.times do |num_b|
+          type = "slider"
+          meta = { min: 0, max: 10 }
+          if random_num % 2 == 0
+            type = "switch"
+            meta = { left: "No", right: "Yes" }
+          end
+          survey.survey_questions << SurveyQuestion.create!(
+            question: "Question " + num_b.to_s + ": " + Faker::Lorem.sentence,
+            answer_type: type,
+            answer_meta: meta,
+            active: true,
+            company_id: company.id,
+            dynamic: false,
+            dynamic_meta: [],
+          )
+        end
+        survey.save
+      end
+      Product.create!(
+        name: 'Product ' + num.to_s,
+        price: random_num(true),
+        company_id: company.id,
+      )
+      Reward.create!(
+        company_id: company.id,
+        cost: random_num,
+        description: Faker::Lorem.sentence,
+        title: Faker::Lorem.word,
+      )
+    end
+  end
+
+  10.times do
+    member = Member.create!(
+      email: Faker::Internet.email,
+      salt: Faker::Lorem.word,
+      other_id: Faker::Lorem.word,
+      active: true,
+    )
+    password = Digest::SHA256.new
+    password.update member.other_id
+    new_password = Digest::SHA256.new
+    new_password.update password.hexdigest + member.salt
+    member.password = new_password.hexdigest
+
+    Company.all.each do |company|
+      points = random_num
+      MemberPoints.create!(
+        company_id: company.id,
+        last_earned: Time.now.utc,
+        member_id: member.id,
+        points: points,
+        total_points: points + random_num,
+      )
+      company.stores.each do |store|
+        10.times do
+          code = Code.create!(
+            qr: Faker::Lorem.characters(32),
+            active: true,
+            store_id: store.id,
+          )
+          order = Order.create!(
+            code_id: code.id,
+            company_id: company.id,
+            store_id: store.id,
+            amount: random_num(true),
+            survey_worth: random_num(true),
+            checkin_worth: random_num(true),
+            server: Faker::Name.name,
+          )
+          2.times do
+            product = company.products.offset(rand(company.products.count)).first
+            OrderDetail.create!(
+              order_id: order.id,
+              product_id: product.id,
+              name: product.name,
+              quantity: random_num,
+              discount: random_num(true),
+              code_id: code.id,
+              price: random_num(true),
+            )
+          end
+        end
+      end
+    end
+
+    member.save
+  end
+when 'production'
+  # Real seed
+
+  ApiKey.delete_all
+  ApiKey.create!
+
   company = Company.create!(
-    name: Faker::Company.name,
+    name: 'Panda Express',
     logo: { 
-      large: 'bigimage', 
-      small: 'smallimage',
-      bg: 'bgimage',
+      main: 'https://s3-us-west-2.amazonaws.com/com.yella.company/panda/pandaLogo@2x.png',
+      bg: 'https://s3-us-west-2.amazonaws.com/com.yella.company/panda/background-fade_pink@2x.png',
       colors: {
         header: {
-          r: random_num,
-          g: random_num,
-          b: random_num,
+          r: 234,
+          g: 88,
+          b: 79,
         },
         points: {
-          r: random_num,
-          g: random_num,
-          b: random_num,
+          r: 234,
+          g: 88,
+          b: 79,
         },
         reward: {
-          r: random_num,
-          g: random_num,
-          b: random_num,
+          r: 234,
+          g: 88,
+          b: 79,
         },
       },
     },
-    description: Faker::Lorem.sentence,
-    location: Faker::Address.street_address(true),
-    phone: Faker::PhoneNumber.phone_number,
     survey_question_limit: 6,
     html: '<!DOCTYPE html><html><head></head><body></body><p style="text-align:center;">Yella <strong>bold</strong> <em>italic</em>!</p><p style="text-align:center;width:100%;">{QR}</p><div style="float:left;width:20%;">float left</div><div style="float:right;width:20%;">float right</div><div style="clear:both;">Cleared</div><div><p>Mongoose:</p><hr /><img src="http://www.enchantedlearning.com/ygifs/Yellowmongoose_bw.GIF" /></div><div><ul><li>A list item</li><li style="color:red;">Print red?</li><li><ul><li>A nested list item</li></ul></li></ul></div><table><tbody><tr><td>Support table cells?</td><td>Support table cells?</td></tr></tbody></table></html>',
   )
-  5.times do |num|
-    store = Store.create!(
-      address1: Faker::Address.street_address,
-      city: Faker::Address.city,
-      company_id: company.id,
-      country: Faker::Address.country,
-      latitude: Faker::Address.latitude,
-      longitude: Faker::Address.longitude,
-      name: 'Store ' + num.to_s,
-      phone: Faker::PhoneNumber.phone_number,
-      state: Faker::Address.state,
-      zip: Faker::Address.zip_code,
-    )
-    5.times do |num_a|
-      survey = Survey.create!(
-        title: "Survey Type " + num_a.to_s,
-        store_id: store.id,
-        description: Faker::Lorem.sentence,
-        default: false,
-      )
-      10.times do |num_b|
-        type = "slider"
-        meta = { min: 0, max: 10 }
-        if random_num % 2 == 0
-          type = "switch"
-          meta = { left: "No", right: "Yes" }
-        end
-        survey.survey_questions << SurveyQuestion.create!(
-          question: "Question " + num_b.to_s + ": " + Faker::Lorem.sentence,
-          answer_type: type,
-          answer_meta: meta,
-          active: true,
-          company_id: company.id,
-          dynamic: false,
-          dynamic_meta: [],
-        )
-      end
-      survey.save
-    end
-    Product.create!(
-      name: 'Product ' + num.to_s,
-      price: random_num(true),
-      company_id: company.id,
-    )
-    Reward.create!(
-      company_id: company.id,
-      cost: random_num,
-      description: Faker::Lorem.sentence,
-      title: Faker::Lorem.word,
-    )
-  end
-end
 
-10.times do
-  member = Member.create!(
-    email: Faker::Internet.email,
-    salt: Faker::Lorem.word,
-    other_id: Faker::Lorem.word,
-    active: true,
+  # get some real data for the following
+
+  store = Store.create!(
+    address1: Faker::Address.street_address,
+    city: Faker::Address.city,
+    company_id: company.id,
+    country: Faker::Address.country,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    name: 'Store 1',
+    phone: Faker::PhoneNumber.phone_number,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip_code,
   )
-  password = Digest::SHA256.new
-  password.update member.other_id
-  new_password = Digest::SHA256.new
-  new_password.update password.hexdigest + member.salt
-  member.password = new_password.hexdigest
 
-  Company.all.each do |company|
-    points = random_num
-    MemberPoints.create!(
-      company_id: company.id,
-      last_earned: Time.now.utc,
-      member_id: member.id,
-      points: points,
-      total_points: points + random_num,
-    )
-    company.stores.each do |store|
-      10.times do
-        code = Code.create!(
-          qr: Faker::Lorem.characters(32),
-          active: true,
-          store_id: store.id,
-        )
-        order = Order.create!(
-          code_id: code.id,
-          company_id: company.id,
-          store_id: store.id,
-          amount: random_num(true),
-          survey_worth: random_num(true),
-          checkin_worth: random_num(true),
-          server: Faker::Name.name,
-        )
-        2.times do
-          product = company.products.offset(rand(company.products.count)).first
-          OrderDetail.create!(
-            order_id: order.id,
-            product_id: product.id,
-            name: product.name,
-            quantity: random_num,
-            discount: random_num(true),
-            code_id: code.id,
-            price: random_num(true),
-          )
-        end
-      end
-    end
-  end
+  Reward.create!(
+    company_id: company.id,
+    title: 'Free Eggroll',
+    description: 'One free chicken or spring eggroll.',
+    cost: 5,
+  )
 
-  member.save
+  Reward.create!(
+    company_id: company.id,
+    title: 'Free Entree',
+    description: 'One free entree of your choice.',
+    cost: 10,
+  )
+
+  Reward.create!(
+    company_id: company.id,
+    title: 'Free Panda Bowl',
+    description: 'One free Panda Bowl. Comes with one entree and one side.',
+    cost: 15,
+  )
+
+  Reward.create!(
+    company_id: company.id,
+    title: 'Free 2 Entree Plate',
+    description: 'One free 2 entree plate. Comes with a side of your choice.',
+    cost: 20,
+  )
+
+  Reward.create!(
+    company_id: company.id,
+    title: 'Free T-Shirt',
+    description: 'One free Panda Express T-Shirt! Tell you cashier which size you would like!',
+    cost: 25,
+  )
+
+  Reward.create!(
+    company_id: company.id,
+    title: 'Free Stuffed Panda Bear',
+    description: 'Get a free cute stuffed Panda Express teddy bear!',
+    cost: 30,
+  )
+
 end
+
