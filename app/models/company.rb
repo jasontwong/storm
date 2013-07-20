8a19ee6d74241c75b4743ccaa5b01dc73446036f
Jason T. Wong <developer@jasontwong.com>
2013-07-20 16:22:15 -0500

    Added relationship between company and surveys

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,6 +7,7 @@ class Company < ActiveRecord::Base
   has_many :products, inverse_of: :company
   has_many :rewards, inverse_of: :company
   has_many :orders, inverse_of: :company
+  has_many :surveys, inverse_of: :company
   has_many :survey_questions, inverse_of: :company
   has_many :member_points, inverse_of: :company
   has_many :member_surveys, inverse_of: :company
