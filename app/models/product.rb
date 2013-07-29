1e6312a0ebfa25545ba2ba337eac4eded547f67f
Jason T. Wong <developer@jasontwong.com>
2013-07-29 13:05:44 -0500

    Added relationship between product and product categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
-  attr_accessible :company_id, :name, :price, :size
+  attr_accessible :company_id, :name, :price, :size, :product_category_id
 
+  belongs_to :product_category, inverse_of: :products
   belongs_to :company, inverse_of: :products
   has_and_belongs_to_many :survey_questions
   has_many :order_details, inverse_of: :product
