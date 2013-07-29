17c24c3bc58571be03534874aed202f550a29260
Jason T. Wong <developer@jasontwong.com>
2013-07-29 13:25:53 -0500

    Added self reference to product for parent/children relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :company_id, :name, :price, :size, :product_category_id
 
+  belongs_to :parent, :class_name => "Product"
   belongs_to :product_category, inverse_of: :products
   belongs_to :company, inverse_of: :products
   has_and_belongs_to_many :survey_questions
+  has_many :children, :class_name => "Product", :foreign_key => 'parent_id'
   has_many :order_details, inverse_of: :product
   has_many :orders, through: :order_details
 
