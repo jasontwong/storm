fd73b4ba7cacb50a6892732a7a920747ee07679e
Jason T. Wong <developer@jasontwong.com>
2013-08-01 10:45:44 -0500

    Changed find and order by created_at to modified_at

--- app/controllers/changelogs_controller.rb
+++ app/controllers/changelogs_controller.rb
@@ -2,10 +2,10 @@ class ChangelogsController < ApplicationController
   # GET /changelogs
   # GET /changelogs.json
   def index
-    @changelogs = Changelog.order('created_at ASC')
+    @changelogs = Changelog.order('modified_at ASC')
 
     if params[:date]
-      @changelogs = @changelogs.where('created_at > ?', params[:date])
+      @changelogs = @changelogs.where('modified_at > ?', params[:date])
     else
       @changelogs = @changelogs.all
     end
