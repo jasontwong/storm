638f58d737d83d454fca75272699f0289d804c01
Jason T. Wong <developer@jasontwong.com>
2013-08-28 10:52:09 -0500

    Updated fb_verify code to allow for FB non-email verification

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -87,7 +87,12 @@ class MembersController < ApplicationController
   # POST /members/fb_verify
   # POST /members/fb_verify.json
   def fb_verify
-    @member = Member.where(email: params[:email], fb_id: params[:fb_id]).first
+    unless params[:email].nil?
+      @member = Member.where(email: params[:email], fb_id: params[:fb_id]).first
+    end
+
+    @member ||= Member.where(fb_id: params[:fb_id]).first
+
     if @member
       render json: @member
     else
