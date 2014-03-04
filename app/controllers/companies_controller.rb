ea573aeff177053c067fb7c15265662ec7fecfcc
Jason T. Wong <developer@jasontwong.com>
2014-03-04 01:01:05 -0600

    Added rewards by default when getting a single company.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -27,8 +27,9 @@ class CompaniesController < ApplicationController
     end
 
     params[:include] = [] unless params[:include].is_a? Array
+    params[:include] << "rewards"
 
-    render json: @company.to_json(:include => params[:include].collect { |data| data.to_sym })
+    render json: @company.to_json(:include => params[:include].uniq!.collect { |data| data.to_sym })
   end
 
   # POST /companies
