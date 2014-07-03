27cae764ffbf48ae6474300581c2c1602a87d08f
Jason T. Wong <developer@jasontwong.com>
2014-07-03 15:38:01 -0500

    Added index to Company model for active column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140410193958) do
+ActiveRecord::Schema.define(:version => 20140703203552) do
 
   create_table "api_keys", :force => true do |t|
     t.string   "access_token"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20140410193958) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "temp_password"
+    t.string   "stripe_id"
   end
 
   create_table "clients_stores", :id => false, :force => true do |t|
@@ -106,6 +107,8 @@ ActiveRecord::Schema.define(:version => 20140410193958) do
     t.text     "worth_meta"
     t.boolean  "active"
   end
+
+  add_index "companies", ["active"], :name => "index_companies_on_active"
 
   create_table "member_answers", :force => true do |t|
     t.integer  "member_id"
