33e2239b6246823225b436c166327f5746e39567
Jason T. Wong <developer@jasontwong.com>
2014-08-04 18:53:26 -0500

    Removed writing temporary file first.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -150,18 +150,13 @@ class CompaniesController < ApplicationController
     
     bucket_name = ENV['AWS_STORM_BUCKET']
     file_name = 'active-companies.json'
-    local_file_path = '/tmp/' + file_name
-
-    File.open(local_file_path, 'w') do |file| 
-      file.write(Company.where(active: true).to_json(include: [:rewards, :stores]))
-    end
-    
+          
     # Get an instance of the S3 interface.
     s3 = AWS::S3.new
 
     # Upload a file.
     key = File.basename(file_name)
-    s3.buckets[bucket_name].objects[key].write(Pathname.new(local_file_path))
+    s3.buckets[bucket_name].objects[key].write(Company.where(active: true).to_json(include: [:rewards, :stores]))
   end
 
 end
