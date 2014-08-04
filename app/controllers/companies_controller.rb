0766d1607b8754a0946000019e6d02aeab8760d4
Jason T. Wong <developer@jasontwong.com>
2014-08-04 18:39:42 -0500

    Created companies#create_payload and updated routes to reflect that.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -140,4 +140,28 @@ class CompaniesController < ApplicationController
     end
   end
 
+  # GET /companies/create_payload
+  # GET /companies/create_payload.json
+  def create_payload
+    AWS.config(
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_KEY']
+    )
+    
+    bucket_name = ENV['AWS_STORM_BUCKET']
+    file_name = 'active-companies.json'
+    local_file_path = '/tmp/' + file_name
+
+    File.open(local_file_path, 'w') do |file| 
+      file.write(Company.where(active: true).to_json(include: [:rewards, :stores]))
+    end
+    
+    # Get an instance of the S3 interface.
+    s3 = AWS::S3.new
+
+    # Upload a file.
+    key = File.basename(file_name)
+    s3.buckets[bucket_name].objects[key].write(Pathname.new(local_file_path))
+  end
+
 end
