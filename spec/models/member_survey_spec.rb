ec66fe4ac75f03ae33237d125817ce042928c54b
Jason T. Wong <developer@jasontwong.com>
2014-08-19 23:31:52 -0500

    Updated member_survey model spec

--- spec/models/member_survey_spec.rb
+++ spec/models/member_survey_spec.rb
@@ -13,15 +13,12 @@ describe MemberSurvey do
   it 'requires a member' do
     expect(FactoryGirl.build(:member_survey, member: nil)).not_to be_valid
   end
-  it 'requires a order' do
-    expect(FactoryGirl.build(:member_survey, order: nil)).not_to be_valid
-  end
   it 'requires a store' do
     expect(FactoryGirl.build(:member_survey, store: nil)).not_to be_valid
   end
   it 'require completed to be false if not true' do
-    expect(FactoryGirl.build(:member_survey, completed: 5).completed).to be_false
-    expect(FactoryGirl.build(:member_survey, completed: 'a').completed).to be_false
+    expect(FactoryGirl.build(:member_survey, completed: 5).completed).to be_falsey
+    expect(FactoryGirl.build(:member_survey, completed: 'a').completed).to be_falsey
     expect(FactoryGirl.build(:member_survey, completed: 1)).to be_valid
     expect(FactoryGirl.build(:member_survey, completed: 0)).to be_valid
   end
