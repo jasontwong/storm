6fc4fdd311a0408a8b92ba899b02ab8c6f855c40
Jason T. Wong <developer@jasontwong.com>
2014-08-20 16:12:00 -0500

    Bumped ios required version to 2.0.

--- app/controllers/ios_controller.rb
+++ app/controllers/ios_controller.rb
@@ -2,6 +2,6 @@ class IosController < ApplicationController
   # POST /ios/check_version
   # POST /ios/check_version.json
   def check_version
-    render json: { success: params[:version].to_f >= (ENV['MIN_IOS_VERSION'] || 1.5).to_f }
+    render json: { success: params[:version].to_f >= 2.0 }
   end
 end
