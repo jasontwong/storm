43fd01cde792e93286e566747b6f7fe89a9a5a1d
Jason T. Wong <developer@jasontwong.com>
2014-09-02 18:48:07 -0500

    Created store_groups#store_group_params permissions.

--- app/controllers/store_groups_controller.rb
+++ app/controllers/store_groups_controller.rb
@@ -1,4 +1,5 @@
 class StoreGroupsController < ApplicationController
+  # {{{ def index
   # GET /store_groups
   # GET /store_groups.json
   def index
@@ -9,6 +10,8 @@ class StoreGroupsController < ApplicationController
     render json: @store_groups.to_json(:include => params[:include].collect { |data| data.to_sym })
   end
 
+  # }}}
+  # {{{ def show
   # GET /store_groups/1
   # GET /store_groups/1.json
   def show
@@ -19,10 +22,12 @@ class StoreGroupsController < ApplicationController
     render json: @store_group.to_json(:include => params[:include].collect { |data| data.to_sym })
   end
 
+  # }}}
+  # {{{ def create
   # POST /store_groups
   # POST /store_groups.json
   def create
-    @store_group = StoreGroup.new(params[:store_group])
+    @store_group = StoreGroup.new(store_group_params)
 
     if @store_group.save
       render json: @store_group, status: :created, location: @store_group
@@ -31,18 +36,22 @@ class StoreGroupsController < ApplicationController
     end
   end
 
+  # }}}
+  # {{{ def update
   # PATCH/PUT /store_groups/1
   # PATCH/PUT /store_groups/1.json
   def update
     @store_group = StoreGroup.find(params[:id])
 
-    if @store_group.update(params[:store_group])
+    if @store_group.update(store_group_params)
       head :no_content
     else
       render json: @store_group.errors, status: :unprocessable_entity
     end
   end
 
+  # }}}
+  # {{{ def destroy
   # DELETE /store_groups/1
   # DELETE /store_groups/1.json
   def destroy
@@ -51,4 +60,13 @@ class StoreGroupsController < ApplicationController
 
     head :no_content
   end
+
+  # }}}
+  private
+    # {{{ def store_group_params
+    def store_group_params
+      params.require(:store_group).permit(:name)
+    end
+
+    # }}}
 end
