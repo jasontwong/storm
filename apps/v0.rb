f60cc4c22f29b675bba214f19f71d3d2326fac14
Jason T. Wong <developer@jasontwong.com>
2014-10-07 18:53:02 -0500

    Added in orchestrate and version upgrade error handling

--- apps/v0.rb
+++ apps/v0.rb
@@ -5,6 +5,8 @@ module Api
   class V0 < Api::Base
     # {{{ before xhr: true, provides: :json do
     before xhr: true, provides: :json do
+      error 426 if !request.env['HTTP_X_IOS_SDK_VERSION'].nil? && request.env['HTTP_X_IOS_SDK_VERSION'].to_f < 1.7
+      error 426 if !request.env['HTTP_X_ANDROID_SDK_VERSION'].nil? && request.env['HTTP_X_ANDROID_SDK_VERSION'].to_f < 1.7
       @O_APP = Orchestrate::Application.new(ENV['ORCHESTRATE_API_KEY'])
       @O_CLIENT = Orchestrate::Client.new(ENV['ORCHESTRATE_API_KEY'])
     end
